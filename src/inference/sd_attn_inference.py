0abeeb56da2c3c276ac5f3133273c635044334ec
XavierJiezou <xuechaozou@foxmail.com>
2025-03-12 14:59:31 +0800

    add stable diffusion attention

---
+++ src/inference/sd_attn_inference.py
@@ -0,0 +1,154 @@
+import warnings
+warnings.filterwarnings('ignore')
+import argparse
+import torch
+import os
+import imageio
+import shutil
+from src.models.diffusion_module import DiffusionLitModule
+import hydra
+from matplotlib import pyplot as plt
+from transformers import CLIPTokenizer
+from omegaconf import OmegaConf
+from diffusers import StableDiffusionPipeline
+from transformers import CLIPFeatureExtractor, CLIPTextModel
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--prompt", type=str, default="She is wearing lipstick. She is attractive and has straight hair.")
+    parser.add_argument("--ckpt_path", type=str, default=None)
+    parser.add_argument(
+        "--model_config", type=str, default="configs/model/stable_diffusion_attn.yaml"
+    )
+    parser.add_argument(
+        "--output_dir",
+        type=str,
+        default="outputs/stable_diffusion_attn",
+    )
+    parser.add_argument("--image_name",type=str,default="27000.png")
+    parser.add_argument(
+        "--tokenizer_id", type=str, default="checkpoints/stablev15"
+    )
+    parser.add_argument("--seed", type=int, default=42)
+    parser.add_argument("--device", type=str, default="cuda")
+    parser.add_argument("--guidance_scale", type=int, default=7.5)
+    parser.add_argument("--height", type=int, default=512)
+    parser.add_argument("--width", type=int, default=512)
+    parser.add_argument("--num_inference_steps", type=int, default=50)
+    parser.add_argument(
+        "--save_denoising",
+        action="store_true",
+        help="Whether to save the denoising results",
+    )
+    parser.add_argument("--tmp_dir", type=str, default="tmp")
+    parser.add_argument("--duration", type=int, default=1)
+    args = parser.parse_args()
+    return args
+
+
+def get_gif(args):
+    filenames = [
+        os.path.join(args.tmp_dir, f"{i}.png")
+        for i in range(0, args.num_inference_steps)
+    ]
+    save_path = os.path.join(args.output_dir, f"denoising_process_{args.image_name}.gif")
+    with imageio.get_writer(save_path, mode="I", duration=args.duration) as writer:
+        for filename in filenames:
+            im = imageio.imread(filename)
+            writer.append_data(im)
+    print(f"{save_path} has saved!")
+    shutil.rmtree(args.tmp_dir)
+    print(f"temp dir has been removed!")
+
+
+def get_pipeline(args):
+    ckpt = None
+    if args.ckpt_path is not None:
+        ckpt = torch.load(args.ckpt_path, map_location=args.device)
+    model_config = OmegaConf.load(args.model_config)  # 加载model config file
+    model: DiffusionLitModule = hydra.utils.instantiate(model_config)
+    if ckpt is not None:
+        model.load_state_dict(ckpt["state_dict"])
+    tokenizer = CLIPTokenizer.from_pretrained(
+        args.tokenizer_id,
+        subfolder="tokenizer",
+    )
+    feature_extractor = CLIPFeatureExtractor.from_pretrained(args.tokenizer_id,subfolder="feature_extractor")
+    pipeline = StableDiffusionPipeline(
+        vae=model.vae,
+        text_encoder=model.text_encoder,
+        unet=model.unet,
+        feature_extractor=feature_extractor,
+        tokenizer=tokenizer,
+        scheduler=model.diffusion_schedule,
+        safety_checker=None,
+    ).to(args.device)
+    del ckpt
+    del model
+    return pipeline
+
+
+def main():
+    args = get_args()
+    print("init pipeline...")
+    pipeline: StableDiffusionPipeline = get_pipeline(args)
+    print("start inference...")
+    generator = torch.Generator().manual_seed(args.seed)
+
+    def decode_tensors(pipe, step, timestep, callback_kwargs):
+        latents = callback_kwargs["latents"]
+        image = pipe.vae.decode(
+            latents / pipe.vae.config.scaling_factor,
+            return_dict=False,
+            generator=generator,
+        )[0]
+        image, has_nsfw_concept = pipe.run_safety_checker(
+            image, args.device, torch.float32
+        )
+        if has_nsfw_concept is None:
+            do_denormalize = [True] * image.shape[0]
+        else:
+            do_denormalize = [not has_nsfw for has_nsfw in has_nsfw_concept]
+        image = pipe.image_processor.postprocess(
+            image, output_type="pil", do_denormalize=do_denormalize
+        )[0]
+
+        tmp_save_path = os.path.join(args.tmp_dir, f"{step}.png")
+
+        plt.imshow(image)
+        plt.title(f"step:{step+1}")
+        plt.axis("off")
+        plt.savefig(tmp_save_path)
+        plt.close()
+        return callback_kwargs
+
+    os.makedirs(args.output_dir, exist_ok=True)
+    if args.save_denoising:
+        print(f"create temp dir:{args.tmp_dir}")
+        os.makedirs(args.tmp_dir, exist_ok=True)
+        image = pipeline.__call__(
+            prompt=args.prompt,
+            height=args.height,
+            width=args.width,
+            num_inference_steps=args.num_inference_steps,
+            generator=generator,
+            callback_on_step_end=decode_tensors,
+            callback_on_step_end_tensor_inputs=["latents"],
+        ).images[0]
+        get_gif(args)
+    else:
+        image = pipeline.__call__(
+            args.prompt,
+            height=args.height,
+            width=args.width,
+            num_inference_steps=args.num_inference_steps,
+            generator=generator,
+        ).images[0]
+    output_path = os.path.join(args.output_dir, args.image_name)
+    image.save(output_path)
+    print(f"done.image saved to {output_path}")
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/models/stable_diffusion_attn.py
@@ -0,0 +1,232 @@
+from typing import Any, Dict, Tuple
+
+import torch
+from lightning import LightningModule
+from torchmetrics import MaxMetric, MeanMetric
+from torchmetrics.classification.accuracy import Accuracy
+from diffusers import (
+    UNet2DConditionModel,
+    DDPMScheduler,
+    AutoencoderKL,
+)
+import torch.nn.functional as F
+from transformers import CLIPTextModel, CLIPVisionModelWithProjection
+import numpy as np
+from torch import nn as nn
+
+
+
+class StableDiffusionAttnLitModule(LightningModule):
+
+    def __init__(
+        self,
+        unet: UNet2DConditionModel | str,
+        diffusion_schedule: DDPMScheduler | str,
+        vae: AutoencoderKL | str,
+        text_encoder: CLIPTextModel | str,
+        optimizer: torch.optim.Optimizer,
+        compile: bool,
+    ) -> None:
+        """Initialize a `MNISTLitModule`.
+
+        :param net: The model to train.
+        :param optimizer: The optimizer to use for training.
+        :param scheduler: The learning rate scheduler to use for training.
+        """
+        super().__init__()
+
+        # this line allows to access init params with 'self.hparams' attribute
+        # also ensures init params will be stored in ckpt
+        self.save_hyperparameters(
+            logger=False,
+            ignore=[
+                "unet",
+                "diffusion_schedule",
+                "vae",
+                "text_encoder",
+            ],
+        )
+
+        self.unet = unet
+        self.diffusion_schedule = diffusion_schedule
+        self.vae = vae
+        self.text_encoder = text_encoder
+
+        if isinstance(unet, str):
+            self.unet = UNet2DConditionModel.from_pretrained(unet, subfolder="unet")
+
+        if isinstance(diffusion_schedule, str):
+            self.diffusion_schedule = DDPMScheduler.from_pretrained(
+                diffusion_schedule, subfolder="scheduler"
+            )
+
+        if isinstance(vae, str):
+            self.vae = AutoencoderKL.from_pretrained(vae, subfolder="vae")
+
+        if isinstance(text_encoder, str):
+            self.text_encoder = CLIPTextModel.from_pretrained(
+                text_encoder, subfolder="text_encoder"
+            )
+        
+        self.unet = self.process_unet(self.unet)
+        self.vae.requires_grad_(False)
+        self.text_encoder.requires_grad_(False)
+
+        # for tracking best so far validation accuracy
+        # for averaging loss across batches
+        self.train_loss = MeanMetric()
+        self.val_loss = MeanMetric()
+        self.test_loss = MeanMetric()
+
+    def process_unet(self, unet: UNet2DConditionModel):
+        unet.requires_grad_(False)
+        # init adapter modules
+        for name,p in unet.named_parameters():
+            if "attn2.to_q" in name or "attn2.to_v" in name:
+                p.requires_grad_(True)
+        return unet
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """Perform a forward pass through the model `self.net`.
+
+        :param x: A tensor of images.
+        :return: A tensor of logits.
+        """
+        return x
+
+    def on_train_start(self) -> None:
+        """Lightning hook that is called when training begins."""
+        # by default lightning executes validation step sanity checks before training starts,
+        # so it's worth to make sure validation metrics don't store results from these checks
+        self.val_loss.reset()
+        self.train_loss.reset()
+        self.test_loss.reset()
+
+    def model_step(
+        self, batch: Tuple[torch.Tensor, torch.Tensor]
+    ) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+        """Perform a single model step on a batch of data.
+
+        :param batch: A batch of data (a tuple) containing the input tensor of images and target labels.
+
+        :return: A tuple containing (in order):
+            - A tensor of losses.
+            - A tensor of predictions.
+            - A tensor of target labels.
+        """
+        with torch.no_grad():
+            latents = self.vae.encode(
+                batch["instance_images"], return_dict=True
+            ).latent_dist.sample()
+            latents = latents * self.vae.config.scaling_factor
+
+        noise = torch.randn_like(latents)
+        bs = latents.shape[0]
+        timesteps = torch.randint(
+            0,
+            self.diffusion_schedule.config.num_train_timesteps,
+            (bs,),
+            device=latents.device,
+        ).long()
+        noised_latents = self.diffusion_schedule.add_noise(latents, noise, timesteps)
+        with torch.no_grad():
+            encoder_hidden_states = self.text_encoder(batch["instance_prompt_ids"])[
+                0
+            ]  # encoder_hidden_states shape : bs 8 768
+        
+
+        noise_pred = self.unet.forward(
+            noised_latents, timesteps, encoder_hidden_states
+        ).sample
+        loss = F.mse_loss(noise_pred, noise, reduction="mean")
+
+        return loss
+
+    def training_step(
+        self, batch: Tuple[torch.Tensor, torch.Tensor], batch_idx: int
+    ) -> torch.Tensor:
+        """Perform a single training step on a batch of data from the training set.
+
+        :param batch: A batch of data (a tuple) containing the input tensor of images and target
+            labels.
+        :param batch_idx: The index of the current batch.
+        :return: A tensor of losses between model predictions and targets.
+        """
+        loss = self.model_step(batch)
+
+        # update and log metrics
+        self.train_loss(loss)
+
+        self.log(
+            "train/loss", self.train_loss, on_step=False, on_epoch=True, prog_bar=True
+        )
+
+        # return loss or backpropagation will fail
+        return loss
+
+    def on_train_epoch_end(self) -> None:
+        "Lightning hook that is called when a training epoch ends."
+        pass
+
+    def validation_step(
+        self, batch: Tuple[torch.Tensor, torch.Tensor], batch_idx: int
+    ) -> None:
+        """Perform a single validation step on a batch of data from the validation set.
+
+        :param batch: A batch of data (a tuple) containing the input tensor of images and target
+            labels.
+        :param batch_idx: The index of the current batch.
+        """
+        loss = self.model_step(batch)
+
+        # update and log metrics
+        self.val_loss(loss)
+        self.log("val_loss", self.val_loss, on_step=False, on_epoch=True, prog_bar=True)
+
+    def test_step(
+        self, batch: Tuple[torch.Tensor, torch.Tensor], batch_idx: int
+    ) -> None:
+        """Perform a single test step on a batch of data from the test set.
+
+        :param batch: A batch of data (a tuple) containing the input tensor of images and target
+            labels.
+        :param batch_idx: The index of the current batch.
+        """
+        loss = self.model_step(batch)
+
+        # update and log metrics
+        self.test_loss(loss)
+        self.log(
+            "test_loss", self.test_loss, on_step=False, on_epoch=True, prog_bar=True
+        )
+
+    def on_test_epoch_end(self) -> None:
+        """Lightning hook that is called when a test epoch ends."""
+        pass
+
+    def setup(self, stage: str) -> None:
+        """Lightning hook that is called at the beginning of fit (train + validate), validate,
+        test, or predict.
+
+        This is a good hook when you need to build models dynamically or adjust something about
+        them. This hook is called on every process when using DDP.
+
+        :param stage: Either `"fit"`, `"validate"`, `"test"`, or `"predict"`.
+        """
+        pass
+
+    def configure_optimizers(self) -> Dict[str, Any]:
+        """Choose what optimizers and learning-rate schedulers to use in your optimization.
+        Normally you'd need one. But in the case of GANs or similar you might have multiple.
+
+        Examples:
+            https://lightning.ai/docs/pytorch/latest/common/lightning_module.html#configure-optimizers
+
+        :return: A dict containing the configured optimizers and learning-rate schedulers to be used for training.
+        """
+        trainable_params = [
+            p for p in self.trainer.model.parameters() if p.requires_grad
+        ]
+        optimizer = self.hparams.optimizer(params=trainable_params)
+        return {"optimizer": optimizer}
+
